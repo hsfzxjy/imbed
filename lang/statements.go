4a14c7d118bab1a56440ba5a080f0ed1557ed638
hsfzxjy <hsfzxjy@gmail.com>
2023-10-09 21:24:37 +0800

    ParseRun_AddBody() should open Tx in a larger scope

--- lang/statements.go
+++ lang/statements.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hsfzxjy/imbed/asset"
+	"github.com/hsfzxjy/imbed/db"
 	"github.com/hsfzxjy/imbed/db/assetq"
 )
 
@@ -12,15 +13,22 @@ func (c *Context) ParseRun_AddBody() ([]asset.StockAsset, error) {
 	if err != nil {
 		return nil, err
 	}
-	graph, err := c.parseTransSeq()
-	if err != nil {
-		return nil, err
-	}
-	initialAsset, err := initialLoader.Do(c.app.DB())
-	if err != nil {
-		return nil, err
-	}
-	assets, err := graph.Compute(c.app, initialAsset)
+	var assets []asset.Asset
+	err = c.app.DB().RunR(func(h db.Context) error {
+		graph, err := c.parseTransSeq()
+		if err != nil {
+			return err
+		}
+		initialAsset, err := initialLoader.Do(h)
+		if err != nil {
+			return err
+		}
+		assets, err = graph.Compute(h, c.app, initialAsset)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
 		return nil, err
 	}
