e002347c47d6c900450901256d359b98db7b54fe
hsfzxjy <hsfzxjy@gmail.com>
2023-10-09 20:14:40 +0800

    Fix ICBytes

--- util/icbytes.go
+++ util/icbytes.go
@@ -5,6 +5,8 @@ import (
 	"encoding/hex"
 )
 
+// ICBytes represents a byte slice that might hold a 4-bit item at the rear.
+// E.g., a hex string `deadb` with odd length could be parsed into ICBytes.
 type ICBytes struct {
 	slice []byte
 }
@@ -19,10 +21,12 @@ func NewICBytes(repr string) (ICBytes, error) {
 		reprSlice[n] = '0'
 		reprSlice[n+1] = '0'
 	} else {
-		m := n + 1
+		m := n + 3
 		reprSlice = make([]byte, m)
 		copy(reprSlice[:n], repr[:])
-		reprSlice[n] = '1'
+		reprSlice[n] = '0'
+		reprSlice[n+1] = '0'
+		reprSlice[n+2] = '1'
 	}
 	buf := make([]byte, len(reprSlice)/2)
 	_, err := hex.Decode(buf, reprSlice)
@@ -36,15 +40,17 @@ func (i ICBytes) PrefixMatch(target []byte) bool {
 	n := len(i.slice)
 	last := i.slice[n-1]
 	if last == 0 {
+		// even hex digits
 		return bytes.HasPrefix(target, i.slice[:n-1])
 	} else {
-		if len(target) < n {
+		// odd hex digits
+		if len(target) <= n-2 {
 			return false
 		}
-		if !bytes.HasPrefix(target, i.slice[:n-1]) {
+		if !bytes.HasPrefix(target, i.slice[:n-2]) {
 			return false
 		}
-		return target[n-1]>>4 == last>>4
+		return target[n-2]>>4 == i.slice[n-2]>>4
 	}
 }
 
