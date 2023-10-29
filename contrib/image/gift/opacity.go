4fdaa607808b7cceeef17882404007200170ddeb
hsfzxjy <hsfzxjy@gmail.com>
2023-10-29 19:11:45 +0800

    Improve opacity

--- contrib/image/gift/opacity.go
+++ contrib/image/gift/opacity.go
@@ -11,6 +11,7 @@ import (
 	"github.com/disintegration/gift"
 	"github.com/hsfzxjy/imbed/schema"
 	"github.com/hsfzxjy/imbed/transform"
+	"github.com/hsfzxjy/imbed/util/rats"
 )
 
 //go:generate go run github.com/hsfzxjy/imbed/schema/gen
@@ -41,14 +42,16 @@ type opacityParams struct {
 	Value *big.Rat `imbed:"v"`
 }
 
-var rat100 = big.NewRat(100, 1)
+func (p *opacityParams) Validate() error {
+	v := p.Value
+	if v.Sign() < 0 || v.Cmp(rats.R100) > 0 {
+		return fmt.Errorf("opacity must be in [0, 100], got %s", v.FloatString(1))
+	}
+	return nil
+}
 
 func (p *opacityParams) BuildTransform(*schema.ZST) (transform.Applier, error) {
-	v := p.Value
-	if v.Sign() < 0 || v.Cmp(rat100) > 0 {
-		return nil, fmt.Errorf("opacity must be in [0, 100], got %s", v.FloatString(1))
-	}
-	f, _ := v.Float64()
+	f, _ := p.Value.Float64()
 	bit := math.Floor(f / 100 * 255)
 	return &opacityApplier{
 		alphaBit: int64(bit),
